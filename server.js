6fd358ae07725455e4765556a5114c1f7466fd97
Namozbey <nostonayev@gmail.com>
2023-03-18 06:01:49 +0500

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // server.js
+const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 2288;
+
+server.use(cors());
 
 server.use(middlewares);
 server.use(router);
